bde140b7a5569acd06cdc9a6170b301b78d3662a
okcaptain <113525318+okcaptain@users.noreply.github.com>
2024-10-29 09:33:41 +0800

    Update Version.h

--- include/Version.h
+++ include/Version.h
@@ -2,7 +2,6 @@
 #include "Instruction.h"
 
 //add version for decoder
-#ifdef linux
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -19,26 +18,4 @@ GENERIC_VERION(1, 0, 0, 6)
 
 #ifdef __cplusplus
 }
-#endif
-#endif
-
-
-#ifdef ARCH_AARCH64
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#define VER_MAJOR				1
-#define VER_MINOR				0
-#define VER_RELEASE				0
-#define VER_BUILD				6
-
-#define GENERIC_VERION(Major, Minor, Release, Build)\
-void VERSION_AVS3AudioDec_##Major##_##Minor##_##Release##_##Build(){ }
-
-GENERIC_VERION(1, 0, 0, 6)
-
-#ifdef __cplusplus
-}
-#endif
 #endif
